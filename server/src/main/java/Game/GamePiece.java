04b72a2647bb54ffd073d6d8ce93da3b0d594eb4
mlnash2 <team.marylou@gmail.com>
2019-09-14 12:15:36 -0600

    Adding GamePiece class.

---
+++ server/src/main/java/Game/GamePiece.java
@@ -0,0 +1,33 @@
+package Game;
+
+public class GamePiece {
+    public int row;
+    public int column;
+    public int player;  /* set to 1 or 2 to indicate which player owns the piece */
+
+    public GamePiece(){
+    }
+
+    public GamePiece(int r, int c, int p){
+        row = r;
+        column = c;
+        player = p;
+    }
+
+    public Boolean inRiver(){
+        /* determines if playing piece is currently in the river */
+        if (row == 4)
+            return true;
+        else
+            return false;
+    }
+
+    public Boolean inRiver(int r){
+        /* determines if specific location is in the river */
+        if (r == 4)
+            return true;
+        else
+            return false;
+    }
+
+}
