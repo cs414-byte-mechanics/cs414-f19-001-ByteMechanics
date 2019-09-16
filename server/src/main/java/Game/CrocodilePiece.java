cb41b9b5c1b24dac37a1f5d9be20cef31952059f
mlnash2 <team.marylou@gmail.com>
2019-09-15 20:24:22 -0600

    Mln game (#33)
    * Adding GamePiece class.
    
    * created zebra,croc,monkey pieces.  Created ValidateMove for monkey

---
+++ server/src/main/java/Game/CrocodilePiece.java
@@ -0,0 +1,16 @@
+package Game;
+
+public class CrocodilePiece extends GamePiece {
+    public CrocodilePiece(){
+    }
+
+    public CrocodilePiece(int row, int col, int player) {
+        super(row, col, player);
+    }
+
+    public boolean ValidateMove(int NextRow, int NextCol) {
+
+
+        return true;
+    }
+}
--- server/src/main/java/Game/GameBoard.java
+++ server/src/main/java/Game/GameBoard.java
@@ -1,9 +1,34 @@
 package Game;
 
 public class GameBoard extends GamePiece {
+    GiraffePiece giraffeP1;
+    MonkeyPiece monkeyP1;
+    CrocodilePiece crocP1;
+    ZebraPiece zebraP1;
 
     GamePiece board[][] = new GamePiece[7][7];
 
+    public GameBoard(){
+     /* initialize every board square to NULL */
+        for (int i = 0; i < 7; i++){
+            for (int j = 0; j < 7; j++) {
+                board[i][j] = null;
+            }
+        }
+    }
 
+    public void InitGameBoard(){
+        /* Create and setup game pieces for player 1 */
+        giraffeP1 = new GiraffePiece();
+        monkeyP1 = new MonkeyPiece(0,1,1);
+        crocP1 = new CrocodilePiece(0,5, 1);
+        zebraP1 = new ZebraPiece(0, 6, 1);
+
+        /* Create and setup game pieces for player 2 */
+        GiraffePiece giraffeP2 = new GiraffePiece();
+        MonkeyPiece monkeyP2 = new MonkeyPiece(6, 1, 2);
+        CrocodilePiece crocP2 = new CrocodilePiece(6, 5, 2);
+        ZebraPiece zebraP2 = new ZebraPiece(6, 6, 2);
+    }
 
 }
