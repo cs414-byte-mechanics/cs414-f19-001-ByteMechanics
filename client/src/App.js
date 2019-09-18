e95db9e96805a2ef22007851b2a5785684d7cdc0
Aislinn Jeske <ajeske11@gmail.com>
2019-09-18 09:26:16 -0600

    Add Forms for User Registration
    The state is saved, but no information is sent to the server yet

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Game from './components/Game';
+import Registration from './components/Registration';
 
 
 function App() {
   return (
     <div className="App">
-      <Game/>
+        <h3>Welcome to Congo!</h3>
+        <Registration/>
     </div>
   );
 }
