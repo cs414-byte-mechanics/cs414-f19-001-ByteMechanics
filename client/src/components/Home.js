69e29a869cd41061e9b13a8ced74ff499ced92b2
Abby Rictor <amrictor@gmail.com>
2019-10-16 00:44:35 -0600

    general styling

---
+++ client/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Home extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render () {
+        return (
+            <div id="home">
+                Welcome to CongoOnline!
+            </div>
+        );
+    }
+}
+
+export default Home;
