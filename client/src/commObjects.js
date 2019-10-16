0f2453723c0fe0bfa53e1e598d6cd8a24cde04b4
Abby Rictor <amrictor@gmail.com>
2019-10-16 00:42:51 -0600

    added commObjects file to export comm objects from as needed

---
+++ client/src/commObjects.js
@@ -0,0 +1,18 @@
+let registerUser = {
+    "communicationType": "registerUser",
+    "communicationVersion": 1,
+    "userName": "",
+    "userEmail": "",
+    "userPassword": ""
+}
+let attemptLogin = {
+    "communicationType": "attemptLogin",
+    "communicationVersion": 1,
+    "userEmail": "",
+    "userPassword": ""
+}
+
+export {
+         registerUser,
+         attemptLogin
+       };
