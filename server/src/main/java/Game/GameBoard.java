9a52ead9979351c12c1152f343486ea755e016de
Aislinn Jeske <ajeske11@gmail.com>
2019-10-25 11:10:42 -0600

    Putting capture piece back

--- server/src/main/java/Game/GameBoard.java
+++ server/src/main/java/Game/GameBoard.java
@@ -177,6 +177,9 @@ public class GameBoard{
         }
     }
 
+    /**
+    Captures the given piece and removes it from the board
+    */
     public void capturePiece(GamePiece pieceToBeCaptured){
         board[pieceToBeCaptured.row][pieceToBeCaptured.column] = null;
     }
