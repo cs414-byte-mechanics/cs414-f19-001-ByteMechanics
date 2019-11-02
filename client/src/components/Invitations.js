9754bd4edd34735abf26be27c5cd538fc5d59dca
zachklau <zachklau@rams.colostate.edu>
2019-11-02 00:05:09 -0600

    stashing changes... * Invitations.js: Added search box and button * DatabaseHandler.java: Fixed issue with wrong variable name for database

--- client/src/components/Invitations.js
+++ client/src/components/Invitations.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Dashboard from "./Dashboard";
+import {Form, FormGroup, Label, Input, Button} from 'reactstrap';
 
 class Invitations extends React.Component {
     constructor(props){
@@ -8,11 +9,42 @@ class Invitations extends React.Component {
 
     render () {
         return (
-            <div id="invitations">
-                View your game invitations and invite other players here.
-            </div>
+          <div>
+            {this.renderText()}
+            <p/><p/><p/>
+            {this.renderSearchBox()}
+          </div>
         );
     }
+
+    renderText() {
+        return (
+          <div id="invitations">
+              View your game invitations and invite other players here!
+          </div>
+        );
+    }
+
+    renderSearchBox() {
+        return (
+          <div>
+            <Form>
+                <FormGroup>
+                    <Label>Search for a User to Invite</Label>
+                    <Input type="searchBox" name="searchBox" id="userSearchBox" placeholder="Enter the name of the user..." />
+                    {this.renderSearchButton()}
+                </FormGroup>
+            </Form>
+          </div>
+        );
+    }
+
+    renderSearchButton() {
+        return (
+          <Button color="info">Search</Button>
+        );
+    }
+
 }
 
 export default Invitations;
