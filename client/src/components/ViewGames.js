6799d71819d7abef0fd2d226b72f8dba20172e17
Aislinn Jeske <ajeske11@gmail.com>
2019-11-21 10:17:20 -0700

    New Component for viewing games

---
+++ client/src/components/ViewGames.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class ViewGames extends Component {
+    //This class needs to:
+        //Get all the games from the server - hard code for now and do later (maybe this is sent back when they've sucessfully logged in?)
+        //Store information in state for now
+        //Display the information on the screen
+        //Make display prettier
+        //Don't hard code
+        //Move state?
+    
+    constructor(props){
+        super(props)
+        this.state={
+            games=[
+                {
+                    date: "yesterday",
+                    opponent: "amrictor",
+                    board: "null"
+                },
+                {
+                    date: "two days ago",
+                    opponent: "fari",
+                    board: "null"
+                },
+                {
+                    date: "Week ago",
+                    opponent: "zach",
+                    board: "null"
+                }
+            ]
+        }
+    }
+    
+    render(){
+        return(
+            <h2>Hello</h2>
+        )
+    }
+}
+
+export default ViewGames
