591c5832661b34ea508957080626976607a395ec
Abigail <amrictor@gmail.com>
2019-11-29 12:58:47 -0700

    fixed validation

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -6,7 +6,7 @@ class Form extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            registerUser: this.props.action
+            submit: this.props.action
         }
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
         this.updateTextField = this.updateTextField.bind(this);
@@ -14,22 +14,22 @@ class Form extends React.Component {
     }
     handleFormSubmit(){
         let patterns = {
-            userName: /[A-Za-z0-9]{0,15}/,
-            userEmail: /[A-Za-z0-9_\-.]{0,15}@[A-Za-z0-9_\-.]{0,15}\.[A-Za-z0-9_\-.]/,
-            userPassword: /[A-Za-z0-9]*/
+            userName: /[A-Za-z0-9]+/,
+            userEmail: /[A-Za-z0-9_\-.]+@[A-Za-z0-9_\-.]+\.[A-Za-z0-9_\-.]+/,
+            userPassword: /[A-Za-z0-9]+/
         }
-        Object.keys(this.state.registerUser).forEach(key => {
-            console.log(typeof this.state.registerUser[key])
-            if (typeof this.state.registerUser[key] == "string" && !this.state.registerUser[key].match(patterns[key])) {
-                alert("Please ensure that you have entered a valid email and your username does not exceed 15 characters.")
+        for (var i = 0; i < Object.keys(this.state.submit).length; i++) {
+            var key = Object.keys(this.state.submit)[i]
+            if (typeof this.state.submit[key] == "string" && !this.state.submit[key].match(patterns[key])) {
+                alert("Please ensure that you have entered a valid email.")
+                return;
             }
-        });
-
-        this.props.sendToServer(this.state.registerUser);
+        }
+        this.props.sendToServer(this.state.submit);
     }
     updateTextField(key, value) {
         let state = this.state;
-        state.registerUser[key] = value;
+        state.submit[key] = value;
         this.setState(state);
     }
 
@@ -37,25 +37,25 @@ class Form extends React.Component {
         if (event.keyCode === 13)
             this.handleFormSubmit(event);
     }
+
     render () {
         return (
-
-                <div id="form">
-                    {this.props.isLoggedIn() ? "You're already logged in!" :
-                    <>
-                        <div className="page_title">{this.props.title}</div>
-                        {Object.keys(this.state.registerUser).filter((key) => key.match(/user/i)).map((field)=>{
-                            let type = field.match(/email/, "gi") ? "email" : field.match(/password/i) ? "password" : "text";
-                            return (
-                                <div className="form_input">
-                                    <Label for={field}>{field.replace("user", "")}: </Label>
-                                    <Input type={type} key={field} onChange={(event)=>{this.updateTextField(field, event.target.value)}} onKeyDown={this.listenForEnter}/>
-                                </div>
-                            );}
-                        )}
-                        <Button block size="sm" onClick={this.handleFormSubmit}>Submit</Button>
-                    </>}
-                </div>
+            <div id="form">
+                {this.props.isLoggedIn() ? "You're already logged in!" :
+                <>
+                    <div className="page_title">{this.props.title}</div>
+                    {Object.keys(this.state.submit).filter((key) => key.match(/user/i)).map((field)=>{
+                        let type = field.match(/email/, "gi") ? "email" : field.match(/password/i) ? "password" : "text";
+                        return (
+                            <div className="form_input">
+                                <Label for={field}>{field.replace("user", "")}: </Label>
+                                <Input type={type} key={field} onChange={(event)=>{this.updateTextField(field, event.target.value)}} onKeyDown={this.listenForEnter}/>
+                            </div>
+                        );}
+                    )}
+                    <Button block size="sm" onClick={this.handleFormSubmit}>Submit</Button>
+                </>}
+            </div>
         );
     }
 }
